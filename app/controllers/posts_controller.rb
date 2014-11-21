2f2d846a0ee4a99689cae45cc3338d9f215088c7
«kamilya-zagrieva» <kamilazm@mail.ru>
2014-11-21 12:32:12 +0400

    New Form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,33 @@ class PostsController <ApplicationController
 	def show
 	end
 
+	def new
+		@post=Post.new 
+	end
+
+	
+
+	def create
+		@post = Post.new (post_params)
+
+		@post.save
+
+		#flash[:notice] = "Post was successfully created"
+
+		redirect_to posts_path, notice: "Post was successfully created"
+	end
+
+
+
 	private
 	 
 	def  post
 		@post ||= Post.find(params[:id])
 	end
 	helper_method :post
+	def post_params
+		params.require(:post).permit(:title,:body)
+	end	
 
 		
 	
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'posts#index'
-  resources :posts, only: [:index,:show]
+  resources :posts, only: [:index,:show,:new,:create]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
